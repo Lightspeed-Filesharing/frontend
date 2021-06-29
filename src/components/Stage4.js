7c777bc43ebc58090d982e2b6dc385653b513940
httpjamesm <github@httpjames.space>
2021-06-29 18:01:54 -0400

    stage4

--- src/components/Stage4.js
+++ src/components/Stage4.js
@@ -7,17 +7,22 @@ import {faCheck} from '@fortawesome/free-solid-svg-icons';
 
 const Stage4 = () => {
     return (
-        <div className="bottom">
-            <div className="titles stage2">
-                <h1 className="titles stage2">File Uploaded</h1>
-                {/* <h2 className="subtitle stage2"></h2> */}
-            </div>
-            <div className="icongroup">
-                <div className="circle stage4">
-                    <FontAwesomeIcon icon={faCheck} />
+        <div className="bottom stage4">
+            <div className="bottom-child">
+                <div className="titles stage2">
+                    <h1 className="title stage4">File Uploaded</h1>
+                    {/* <h2 className="subtitle stage2"></h2> */} </div>
+                <div className="icongroup">
+                    <div className="circle stage4">
+                        <FontAwesomeIcon size="lg" icon={faCheck}/>
+                    </div>
+                </div>
+                <div className="center stage4">
+                    <p className="direction" style={{textAlign: 'center'}}>Your file is ready to be securely shared.</p>
+                    <p className="direction small">Copy your link below.</p>
                 </div>
             </div>
-    </div>
+        </div>
 
     )
 }
